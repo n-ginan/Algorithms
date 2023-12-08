84f8213ead49708dd766625ee7b0a2b004151dc7
Gidan <142613110+papsykle@users.noreply.github.com>
2023-12-08 21:06:39 +0800

    Add files via upload

---
+++ DSA/SortingAlgorithms/InsertionSort.java
@@ -0,0 +1,9 @@
+package DSA.SortingAlgorithms;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        //create outer loop
+        //check if a[0] > a[1] (inner loop 1)
+        //check if a[1] < a[0] (inner loop 2)
+    }
+}
