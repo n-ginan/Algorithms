3c490a86285aad05c101724873fe46a9f799827b
Gidan <142613110+papsykle@users.noreply.github.com>
2023-12-08 21:09:36 +0800

    Adududu

--- DSA/SortingAlgorithms/InsertionSort.java
+++ DSA/SortingAlgorithms/InsertionSort.java
@@ -2,8 +2,45 @@ package DSA.SortingAlgorithms;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        //create outer loop
-        //check if a[0] > a[1] (inner loop 1)
-        //check if a[1] < a[0] (inner loop 2)
+        int[] unsorted = {50, 34, 5, 1, 7};
+        int lesser;
+        System.out.print("Unsorted: ");
+        for (int numbers : unsorted) {
+            System.out.print(numbers + " ");
+        }
+        System.out.println("");
+        for (int i = 0; i < unsorted.length ; i++) {
+            for (int j = 0; j < unsorted.length - 1; j++) { 
+                if (unsorted[j] > unsorted[j + 1]) { 
+                    lesser = unsorted[j + 1];  
+                    unsorted[j + 1] = unsorted[j]; 
+                    unsorted[j] = lesser; 
+                    System.out.print("Right: ");
+                    for (int numbers : unsorted) {
+                        System.out.print(numbers + " ");
+                    }
+                    System.out.println("");
+                    
+                    if (j > 0) {
+                        for (int k = j; k < unsorted.length - 1; k--) {
+                            if (k < 1) {
+                                break;
+                            }
+                            if (unsorted[k - 1] > unsorted[k]) { 
+                                lesser = unsorted[k];
+                                unsorted[k] = unsorted[k - 1];
+                                unsorted[k - 1] = lesser;
+                                System.out.print("Left: ");
+                                for (int numbers : unsorted) {
+                                    System.out.print(numbers + " ");
+                                }
+                                System.out.println("");
+                            }
+                        } 
+                    }
+                }
+            }        
+        }
     }
 }
+
